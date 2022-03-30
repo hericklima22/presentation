69f50079f7e61ddee3fb257ac89c8dacac03904f
hericklima22 <herick.lima77@gmail.com>
2022-03-30 13:03:57 -0300

    novo estilo

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,20 @@
-import TopNavBar from "../../components/TopNavBar/"
 import "./styles.scss"
 
 export const Home = () => {
   return (
     <div className="container">
-      <div>
-        <TopNavBar />
+      <div className="tittle">
+        <div style={{margin: 10}}>
+          <a>{"> Nome: Herick de Carvalho Lima"}</a>
+        </div>
+        <br></br>
+        <div style={{margin: 10}}>
+          <a>{"> Idade: 22"}</a>
+        </div>
+        <br></br>
+        <div style={{margin: 10}}>
+          <a>{"> Main : Desenvolvedor Front-End"}</a>
+        </div>
       </div>
     </div>
   )  
